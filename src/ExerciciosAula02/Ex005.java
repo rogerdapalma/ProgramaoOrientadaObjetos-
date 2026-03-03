14eba27eaa58447a1b8addd440f8c18ce3d7b282
roger <rogerdapalma@gmail.com>
2026-03-03 20:39:39 -0300

    add aula 02 e exercicios feitos em aula

--- src/ExerciciosAula02/Ex005.java
+++ src/ExerciciosAula02/Ex005.java
@@ -1,15 +1,19 @@
 package ExerciciosAula02;
 
+import java.util.Scanner;
+
 public class Ex005 {
 
 	public static void main(String[] args) {
-		int numero = 5;
-		long fatorial = 1;
-		
+		Scanner teclado = new Scanner(System.in);
+		int numero ;
+		int fatorial = 1;
+		System.out.println("digite um numero inteiro");
+		numero = teclado.nextInt();
 		for(int i = 1; i<= numero;i++) {
 			fatorial *=i;
 		}
-		System.out.println("o fatorial de " + numero + "é" + fatorial);
+		System.out.println("o fatorial de " + numero + " é " + fatorial);
 
 	}
 
