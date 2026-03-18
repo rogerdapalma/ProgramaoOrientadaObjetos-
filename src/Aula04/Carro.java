4bc997b2a7670e0860c9d345dee543ab31a2c2d1
roger <rogerdapalma@gmail.com>
2026-03-17 22:15:38 -0300

    add aula 04

---
+++ src/Aula04/Carro.java
@@ -0,0 +1,16 @@
+package Aula04;
+
+public class Carro {
+	public String marca;
+	public String modelo;
+	public int anoFabricacao;
+
+	Carro(String marca, String modelo, int anoFabicacao) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anoFabricacao = anoFabicacao;
+	}
+	public void exibirDados() {
+		System.out.println(marca + "| " + modelo + " | " + anoFabricacao);
+	}
+}
